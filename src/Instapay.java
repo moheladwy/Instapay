b95fe71a9d670ffdd6446b5ce17fa8a384452448
moheladwy <mohamed.h.eladwy@gmail.com>
2023-11-12 13:37:12 +0200

    add some code to Instapay class

--- src/Instapay.java
+++ src/Instapay.java
@@ -1,10 +1,57 @@
+import enums.AuthenticationStatus;
 import logic.*;
 import models.*;
 
+import java.util.function.BinaryOperator;
+
 public class Instapay {
-    User user;
-    Authentication service;
+    public static User user;
+
+    public Instapay() {
+        user = null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Instapay!");
+        Instapay instapay = new Instapay();
+        instapay.start();
+    }
+
+    public void mainMenu() {
+        System.out.println("Welcome to Instapay!");
+        System.out.println("Please choose from the following options=>");
+        System.out.println("==========================================");
+        System.out.println("1- Register.");
+        System.out.println("2- Login.");
+        System.out.println("3- Exit.");
+    }
+
+    public int askUser() {
+        System.out.print("Enter your choice: ");
+        String choice = System.console().readLine();
+        return Integer.parseInt(choice);
+    }
+
+    public void start() {
+        boolean wannaExit = false;
+        while (!wannaExit) {
+            mainMenu();
+            int choice = askUser();
+            AuthenticationStatus status;
+            switch (choice) {
+                case 1:
+                    status = Authentication.register(user);
+                    break;
+                case 2:
+                    status = Authentication.login(user);
+                    break;
+                case 3:
+                    wannaExit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
+        System.out.println("Thank you for using Instapay!");
     }
 }
--- src/logic/Authentication.java
+++ src/logic/Authentication.java
@@ -1,14 +1,15 @@
 package logic;
 import enums.*;
+import models.*;
 
 public class Authentication {
-    public static AuthenticationStatus login() {
+    public static AuthenticationStatus login(User user) {
         return AuthenticationStatus.LOGIN_FAILED;
     }
-    public static AuthenticationStatus register() {
+    public static AuthenticationStatus register(User user) {
         return AuthenticationStatus.REGISTRATION_FAILED;
     }
-    public static AuthenticationStatus sendOTP() {
+    public static AuthenticationStatus sendOTP(User user) {
         return AuthenticationStatus.REGISTRATION_FAILED;
     }
 }
