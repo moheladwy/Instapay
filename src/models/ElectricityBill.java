f5de1dd78169c31ffc84d2c29d5b37c5d8649d1f
Mansour <mansour24@protonmail.com>
2023-11-13 19:50:14 +0200

    Revert "Bills classes done and waiting for testing."
    This reverts commit 3c111f69353d10ef0fe2427d22550de19c84b05e.

--- src/models/ElectricityBill.java
+++ src/models/ElectricityBill.java
@@ -2,26 +2,7 @@ package models;
 import enums.*;
 
 public class ElectricityBill extends Bill {
-    String areaCode;
-    String subscriberNumber;
-    String subscriberName;
-
-    public ElectricityBill(double amount, String paymentNumber, String Name) throws Exception {
+    public ElectricityBill(double amount, String paymentNumber) throws Exception {
         super(amount, paymentNumber);
-        subscriberName = Name;
-        areaCode = paymentNumber.substring(0,3);
-        subscriberNumber = paymentNumber.substring(3,12);
-    }
-
-    public String getAreaCode() {
-        return areaCode;
-    }
-
-    public String getSubscriberNumber() {
-        return subscriberNumber;
-    }
-
-    public String getSubscriberName() {
-        return subscriberName;
     }
 }
