a6780d1460e7994108503a603b654c4f5942ebbc
Mansour <mansour24@protonmail.com>
2023-11-13 20:46:39 +0200

    attributes are now private, JDK 17 is configured, setters now throw exceptions.

--- src/models/ElectricityBill.java
+++ src/models/ElectricityBill.java
@@ -2,9 +2,9 @@ package models;
 import enums.*;
 
 public class ElectricityBill extends Bill {
-    String areaCode;
-    String subscriberNumber;
-    String subscriberName;
+    private String areaCode;
+    private String subscriberNumber;
+    private String subscriberName;
 
     public ElectricityBill(double amount, String paymentNumber, String Name) throws Exception {
         super(amount, paymentNumber);
@@ -23,5 +23,23 @@ public class ElectricityBill extends Bill {
 
     public String getSubscriberName() {
         return subscriberName;
+    }
+
+    public void setSubscriberName(String subscriberName) throws Exception {
+        if (subscriberName == null)
+            throw new Exception("Subscriber Name cannot be null!");
+        this.subscriberName = subscriberName;
+    }
+
+    public void setAreaCode(String areaCode) throws Exception {
+        if (areaCode == null)
+            throw new Exception("Area Code cannot be null!");
+        this.areaCode = areaCode;
+    }
+
+    public void setSubscriberNumber(String subscriberNumber) throws Exception {
+        if (subscriberNumber == null)
+            throw new Exception("Subscriber Number cannot be null!");
+        this.subscriberNumber = subscriberNumber;
     }
 }
--- src/models/WaterBill.java
+++ src/models/WaterBill.java
@@ -2,9 +2,9 @@ package models;
 import enums.*;
 
 public class WaterBill extends Bill {
-    String areaCode;
-    String subscriberNumber;
-    String subscriberName;
+    private String areaCode;
+    private String subscriberNumber;
+    private String subscriberName;
 
     public WaterBill(double amount, String paymentNumber, String Name) throws Exception {
         super(amount, paymentNumber);
@@ -24,5 +24,23 @@ public class WaterBill extends Bill {
 
     public String getSubscriberName() {
         return subscriberName;
+    }
+
+    public void setSubscriberName(String name) throws Exception {
+        if (name == null)
+            throw new Exception("Name cannot be null!");
+        subscriberName = name;
+    }
+
+    public void setAreaCode(String code) throws Exception {
+        if (code == null)
+            throw new Exception("Area Code cannot be null!");
+        areaCode = code;
+    }
+
+    public void setSubscriberNumber(String number) throws Exception {
+        if (number == null)
+            throw new Exception("Subscriber Number cannot be null!");
+        subscriberNumber = number;
     }
 }
